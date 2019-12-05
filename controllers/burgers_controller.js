e09a8d4208081dd0bfc34e83a490e3f1a691c0aa
Khoa Nguyen <kh.nguyen0101@gmail.com>
2019-12-05 11:26:42 -0800

    created the router, untested yet

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,13 +9,13 @@ var burger = require("../models/burger");
 // GET, selectAll
 router.get("/", function(req, res) {
     // run the selectAll function
-    burger.selectAll(function(data) {
+    burger.selectAll(function(result) {
         // Create a variable to hold all burgers
         var allBurgers = {};
 
-        // Assign the data returned from the selectAll function to the key burgers, place into the allBurgers object
+        // Assign the data returned from the selectAll function to a key burgers, place into the allBurgers object
         allBurgers = {
-            burgers: data
+            burgers: result
         };
 
         console.log(allBurgers);
@@ -35,6 +35,8 @@ router.post("/api/burgers", function(req, res) {
 
     // One array for cols
     // One array for vals
+    // req.body = {name: burger1, devoured: true}
+    // req.body[key][0] & req.body[key][1] for cols ???
     burger.insertOne([name, devoured], [valName, valDevoured], function(result) {
     
         // Send back the id of the new burger posted
@@ -44,7 +46,29 @@ router.post("/api/burgers", function(req, res) {
 
 // PUT, updateOne
 router.put("/api/burgers/:id", function(req, res) {
-    //
+    // Get a handle on the id passed, create a variable to hold it
+    var idInterest = req.params.id; 
+
+    // Create a variable to hold the condition, build it using the id obtained above
+    var condition = "id = " + idInterest; 
+    
+    console.log("Condition: " + condition);
+
+    // Get a hande on the body of data being passed
+    var bodyInfo = req.params.body; 
+
+    console.log(bodyInfo);
+
+    // Run the updateOne function
+    burger.updateOne(bodyInfo, condition, function(result) {
+        // If no rows were changed then the id must not exist, return 404 err
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
 
 });
 
